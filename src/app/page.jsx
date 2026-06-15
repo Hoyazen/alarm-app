fe25379ce69ef2f37e924d0f48b7dfa827569639
Hoyazen <hoyazenn@gmail.com>
2026-06-15 16:06:57 +0200

    clock component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import Background from './components/background/background';
 import DarkModeButton from './components/dark-mode-button/darkModeButton';
 import styles from './page.module.css';
 import SongsList from './components/songs-list/songsList';
+import Clock from './components/clock/clock';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
           <div className={styles.app}>
             <DarkModeButton />
             <SongsList />
+            <Clock />
           </div>
       </DarkModeProvider>
     </main>
