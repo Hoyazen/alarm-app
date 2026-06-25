7d1e1714186810cfbd36f3fbd7a0c462da17f815
Ludovic Esperce <ludovic.esperce@afpa.fr>
2026-06-25 14:41:34 +0200

    Ajout d'un préfix au chemin pour production

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+// Si en production
+const productionMode = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  output: 'export',
+  output: 'export', // cette option permet de générer un build static dans "out/"
+  basePath: productionMode ? '/alarm-app' : '',
+  assetPrefix: productionMode ? '/alarm-app/' : '',
   images: { unoptimized: true }, // désactivation de l'optimisation (car uniquement CSR, pas fonctionnel avec Github Page)
   reactCompiler: true,
 };
